4030fbc080868e50b4a1c9e9717d535fcc0ee6aa
João Roberto Lopes da Silva <joao.r.lopesdasilva@gmail.com>
2020-04-13 23:35:54 -0300

    :sparkles: List, create transactions and get balance.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,20 +9,47 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance = {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  };
+
   constructor() {
     this.transactions = [];
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: Transaction): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    if (type === 'income') {
+      this.balance.income += value;
+    } else if (type === 'outcome') {
+      const diff = this.balance.total - value;
+      if (diff < 0) {
+        throw Error('Transaction exceeds balance limits.');
+      }
+      this.balance.outcome += value;
+    }
+
+    this.transactions.push(transaction);
+
+    this.balance.total = this.transactions.reduce((acc, curr) => {
+      if (curr.type === 'income') {
+        return acc + curr.value;
+      }
+      return acc - curr.value;
+    }, 0);
+
+    return transaction;
   }
 }
 
